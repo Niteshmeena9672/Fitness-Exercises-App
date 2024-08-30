b56a3e46502524ed1018e6802486b8fa66bc161f
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-08-30 18:39:41 +0530

    error resolved

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key':"94a3ba706bmsh1805581749279e2p19e610jsna436ae452216"
   },
 };
 
@@ -12,7 +12,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    'X-RapidAPI-Key':"94a3ba706bmsh1805581749279e2p19e610jsna436ae452216"
   },
 };
 
